3ceff222d5b13800de31ef5831b44c1cda5538a2
mdeejay <mdjrussia@gmail.com>
2019-01-07 07:25:26 +0100

    Beast bring up

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from Beast
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/beast/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/beast/sepolicy/qcom/common \
+    device/beast/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
